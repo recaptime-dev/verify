06d575062daa456a2ef78bf881aadbdd95d90625
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-04-20 23:44:57 +0800

    repo: nuke the API directory
    We don't want this repo to be Vercel-only if they want to self-host it.
    So, it's on my todo list on turning this simple API file into full-blown frontend + API server.

--- api/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-    // Just basic OK status.
-    res.json({
-        success: "ok",
-        description: "API server is up"
-        platformStatus: "https://www.vercel-status.com/"});
-}
